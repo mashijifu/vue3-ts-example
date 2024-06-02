734817d68dfcffb292c7002aff2e2115c597de61
mashijifu <4ts0t0i7c@gmail.com>
2024-06-02 18:23:11 +0900

    compositionAPI課題2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,24 @@
 <template>
-  <p>{{ text }}</p>
+  <div class="post-code">
+    <input maxlength="7" type="text" v-model="postCode" />
+    <p>郵便番号：{{ hyphenCode }}</p>
+    <p>日付：{{ displayDate() }}</p>
+  </div>
 </template>
-<script setup lang="ts">
-import { ref } from "vue";
 
-const text = "サンプルテキスト";
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+/* ①データを返す */
+const postCode = ref("");
+
+/* ②算出プロパティを定義する */
+const hyphenCode = computed((): string => {
+  return postCode.value.slice(0, 3) + "-" + postCode.value.slice(3, 7);
+});
+
+/* ③各メソッドを定義する */
+const displayDate = (): any => {
+  return new Date();
+};
 </script>
